d5a04decd8dd08af2fc5f18cdcf34ae38a3490cb
Luna Sofia <nore0404@gmail.com>
2021-01-14 10:43:58 +0000

    Formatting fixes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Hi, this is our first project.
 # So far we do not have any plans or ongoing projects,
 # so if you have a good concept idea, put it in "ideas.txt"
+
 from tkinter import *
 
 class Window(Frame):
